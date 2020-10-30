5858c657653badfa160a37f336aa719859653d2b
Artem Chaylo <achaylo@visiblemagic.com>
2020-10-30 15:22:17 +0200

    init

---
+++ frontity.settings.js
@@ -0,0 +1,53 @@
+const settings = {
+  "name": "my-app",
+  "state": {
+    "frontity": {
+      "url": "https://test.frontity.org",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/mars-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "Happy ",
+              "/happy/"
+            ],
+            [
+              "Page2",
+              "/page-2/"
+            ],
+            [
+              "About Us",
+              "/about-us/"
+            ]
+          ],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "api": "http://10.33.21.21/wp-json"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
